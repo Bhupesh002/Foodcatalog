6299fbb5fa0b5d22214c4c4caf024a3c1c65e2bb
Bhupesh002 <65340489+Bhupesh002@users.noreply.github.com>
2021-07-19 18:56:14 +0530

    Delete cookbook-master directory

--- cookbook-master/javascript.js
+++
@@ -1,9 +0,0 @@
-function texu(){
-    var elm = parseInt(document.getElementById("tex").value);
-    if(isNaN(elm)){
-        alert("Enter a valid amount");
-    }
-    else{
-        window.confirm("Are you sure to donate!");
-    }   
-}
